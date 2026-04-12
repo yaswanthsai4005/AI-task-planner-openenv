e6e5cf0bf973570ffc444a472ad3dc2867504467
KOSANA YASWANTH SAI <yashwanthsai4005@gmail.com>
2026-04-12 20:48:30 +0530

    Change server port from 8000 to 7860

--- server/app.py
+++ server/app.py
@@ -20,7 +20,7 @@ def take_step(plan: dict):
     return result
 
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
